3a9d9526b21b07b3adb1834e347d5bc6f14bd4a2
faustino-gonzalez <61607565+faustino-gonzalez@users.noreply.github.com>
2021-10-26 22:28:16 -0500

    aa

---
+++ aplicacion2/src/aplicacion2/main.java
@@ -0,0 +1,11 @@
+package aplicacion2;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ aplicacion2/src/module-info.java
@@ -0,0 +1,2 @@
+module aplicacion2 {
+}
